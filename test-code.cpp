231a7c9456654b308a7391766c48000caf85b992
brasse18 <brasse.bb@gmail.com>
2022-08-02 03:36:46 +0200

    save

--- test-code.cpp
+++ test-code.cpp
@@ -2,9 +2,10 @@
  
 int main(int argc, char *argv[])
 {
-    double * noruns = [3];
+    double ** noruns = new double * [3];
     noruns[0] = new double [2];
-    noruns[0][0] = 10; 
+    noruns[0][0] = 0.1;
+    noruns[0][1] = 0.1; 
     
     std::cout << noruns[0][0] << std::endl;
     return 0;
