3b59f920414b755961fc7f3d04cdc5f87a3ce9a0
brasse18 <brasse.bb@gmail.com>
2022-08-02 03:31:06 +0200

    save

--- test-code.cpp
+++ test-code.cpp
@@ -2,8 +2,10 @@
  
 int main(int argc, char *argv[])
 {
-    double noruns = [3,6,2];
+    double * noruns = [3];
+    noruns[0] = new double [2];
+    noruns[0][0] = 10; 
     
-    std::cout << tal1 + tal2 << std::endl;
+    std::cout << noruns[0][0] << std::endl;
     return 0;
 }
